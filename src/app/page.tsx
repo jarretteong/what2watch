c71e77f45d45107a8cf0474c8e30652e72ec3644
Jarrette Ong <jarrettevictoriaong@gmail.com>
2023-02-19 17:25:41 +0100

    fix env issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,21 @@ import "./page.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
-  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
-  console.log(process.env.VERCEL_URL)
-    const apiUrl = process.env.API_URL ? `http://${process.env.API_URL}` : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    const data = await fetch(`${apiUrl}/api/movies/popular`);
-    const popularMovies = await data?.json();
+    // const data = await fetch(`${process.env.API_URL}/api/movies/popular`);
+    // const popularMovies = await data?.json();
     
     return (
         <main className="main">
             {/* <Popular movies={popularMovies.results} /> */}
             <div>
-                {popularMovies?.results?.map((movie: any) => {
+                {/* {popularMovies?.results?.map((movie: any) => {
                     return (
                         <div key={movie.id}>
                             <h2>{movie.title}</h2>
                             <h3>{movie.overview}</h3>
                         </div>
                     );
-                })}
+                })} */}
             </div>
         </main>
     );
