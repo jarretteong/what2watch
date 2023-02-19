b2f2002cfa59d715413e239c28de238c893b6c07
Jarrette Ong <jarrettevictoriaong@gmail.com>
2023-02-19 16:28:35 +0100

    fix deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
     const data = await fetch(`${process.env.API_URL}/api/movies/popular`);
-    const popularMovies = await data.json();
+    // const popularMovies = await data?.json();
     
     return (
         <main className="main">
             {/* <Popular movies={popularMovies.results} /> */}
             <div>
-                {popularMovies.results?.map((movie: any) => {
+                {/* {popularMovies?.results?.map((movie: any) => {
                     return (
                         <div key={movie.id}>
                             <h2>{movie.title}</h2>
                             <h3>{movie.overview}</h3>
                         </div>
                     );
-                })}
+                })} */}
             </div>
         </main>
     );
