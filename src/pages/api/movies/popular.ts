c42b843ad8f73b6a36e5b17e58c5ff35df1fe7cb
Jarrette Ong <jarrettevictoriaong@gmail.com>
2023-02-19 17:01:57 +0100

    fix env issues

--- src/pages/api/movies/popular.ts
+++ src/pages/api/movies/popular.ts
@@ -9,6 +9,7 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) => {
+    console.log(req)
     const data = await fetch(`${process.env.TMDB_V3_URL}/movie/popular?api_key=${process.env.TMDB_APIKEY}`);
     const popularMovies = await data.json();
     res.status(200).json(popularMovies);
