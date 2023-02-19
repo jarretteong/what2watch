cc6ca9088c3df72e2d4e0070da5bb3da04988440
Jarrette Ong <jarrettevictoriaong@gmail.com>
2023-02-19 17:20:23 +0100

    logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import "./page.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
+  console.log(process.env.VERCEL_URL)
     const apiUrl = process.env.API_URL ? `http://${process.env.API_URL}` : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     const data = await fetch(`${apiUrl}/api/movies/popular`);
     const popularMovies = await data?.json();
